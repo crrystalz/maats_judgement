068c07824d7fbe287227f49470248faca27ac8dc
crrystalz <lichupatnaik@gmail.com>
2023-11-10 20:38:27 -0800

    restarting no chat gpt cause its bad

--- maats judgement/heart.gd
+++ maats judgement/heart.gd
@@ -1,25 +1,11 @@
 extends Sprite2D
 
-var weight = 1
-var being_dragged = false
-var drag_offset = Vector2()
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	var area = get_node("HeartArea")
-	area.input_pickable = true
-	area.connect("input_event", Callable(self, "_on_HeartArea_input_event"))
+	pass # Replace with function body.
 
-func _on_HeartArea_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			being_dragged = true
-			drag_offset = self.position - event.position
-			self.z_index = 1
-		else:
-			being_dragged = false
-			self.z_index = 0
-			# Add logic to snap the heart to the scales if needed
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if being_dragged:
-		self.position = get_global_mouse_position() + drag_offset
+	pass
