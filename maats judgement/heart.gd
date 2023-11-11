d91d2eebdd9944ff079c67677950517a4500e334
crrystalz <lichupatnaik@gmail.com>
2023-11-10 19:38:43 -0800

    no errors, dragging, no snapping

--- maats judgement/heart.gd
+++ maats judgement/heart.gd
@@ -1,21 +1,18 @@
-extends Sprite
+extends Sprite2D
 
 var weight = 1
 var being_dragged = false
 var drag_offset = Vector2()
 
 func _ready():
-	# Assuming the child Area2D is named "HeartArea"
 	var area = get_node("HeartArea")
-	area.connect("input_event", self, "_on_HeartArea_input_event")
-	area.mouse_filter = Control.MOUSE_FILTER_STOP
-	pass # Replace with function body.
+	area.input_pickable = true
+	area.connect("input_event", Callable(self, "_on_HeartArea_input_event"))
 
 func _on_HeartArea_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			being_dragged = true
-			# Calculate the offset from the sprite's position to the mouse click position
 			drag_offset = self.position - event.position
 			self.z_index = 1
 		else:
@@ -25,5 +22,4 @@ func _on_HeartArea_input_event(viewport, event, shape_idx):
 
 func _process(delta):
 	if being_dragged:
-		# Use the offset so that the dragging feels more natural
 		self.position = get_global_mouse_position() + drag_offset
--- maats judgement/scalesarea.gd
+++ maats judgement/scalesarea.gd
@@ -9,10 +9,11 @@ func _on_ScalesArea_body_entered(body):
 # The function to run when the heart exits the scales area
 func _on_ScalesArea_body_exited(body):
 	if body.is_in_group("draggable"):
+		pass
 		# You can add logic here if you want to do something when the heart leaves the scales area
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Connect the body_entered and body_exited signals to the relevant functions
-	connect("body_entered", self, "_on_ScalesArea_body_entered")
-	connect("body_exited", self, "_on_ScalesArea_body_exited")
+	connect("body_entered", Callable(self, "_on_ScalesArea_body_entered"))
+	connect("body_exited", Callable(self, "_on_ScalesArea_body_exited"))
